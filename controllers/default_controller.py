28a08fed7b198fa52326e09d67f400ae2d3b1ea2
Malathi <malathim993@gmail.com>
2016-03-02 22:25:27 +0530

    python:2.7_created

---
+++ controllers/default_controller.py
@@ -0,0 +1,15 @@
+
+def python_flask_get() -> str:
+    return 'do some magic!'
+
+def python_flask_post() -> str:
+    return 'do some magic!'
+
+def python_flask_python_flask_id_get(pythonFlaskId) -> str:
+    return 'do some magic!'
+
+def python_flask_python_flask_id_put(pythonFlaskId) -> str:
+    return 'do some magic!'
+
+def python_flask_python_flask_id_delete(pythonFlaskId) -> str:
+    return 'do some magic!'
